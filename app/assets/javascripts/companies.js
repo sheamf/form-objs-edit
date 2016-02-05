9367550dfc302024acd54eb9b5db572448b087d3
sheamf <miles_shea@yahoo.com>
2016-02-04 19:44:53 -0700

    add ability to remove additional company rows

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -1,31 +1,46 @@
 var addOfficeRow = function() {
 
-    var $lastOfficeRow = $('.office-row-fieldset').last();
-    var $newOfficeRow = $lastOfficeRow.clone();
-    $newOfficeRow.find('input').val('');
-    var officeRowCount = $('.office-row-fieldset').length;
+  var $lastOfficeRow = $('.office-row-fieldset').last();
+  var $newOfficeRow = $lastOfficeRow.clone();
+  $newOfficeRow.find('input').val('');
+  $newOfficeRow.find('.remove-office-row').show();
+  var officeRowCount = $('.office-row-fieldset').length;
 
-    $newOfficeRow.find('label').each(function() {
-      var oldLabel = $(this).attr('for');
-      var newLabel = oldLabel.replace(new RegExp(/_[0-9]+_/), "_" + officeRowCount + "_");
-      $(this).attr('for', newLabel);
-    });
+  $newOfficeRow.find('label').each(function() {
+    var oldLabel = $(this).attr('for');
+    var newLabel = oldLabel.replace(new RegExp(/_[0-9]+_/), "_" + officeRowCount + "_");
+    $(this).attr('for', newLabel);
+  });
 
-    $newOfficeRow.find('input').each(function() {
-      var oldId = $(this).attr('id');
-      var newId = oldId.replace(new RegExp(/_[0-9]+_/), "_" + officeRowCount + "_");
-      $(this).attr('id', newId);
-      
-      var oldName = $(this).attr('name');
-      var newName = oldName.replace(new RegExp(/\[[0-9]+\]/), "[" + officeRowCount + "]");
-      $(this).attr('name', newName);
-    });
+  $newOfficeRow.find('input').each(function() {
+    var oldId = $(this).attr('id');
+    var newId = oldId.replace(new RegExp(/_[0-9]+_/), "_" + officeRowCount + "_");
+    $(this).attr('id', newId);
 
-    $newOfficeRow.insertAfter($lastOfficeRow);
+    var oldName = $(this).attr('name');
+    var newName = oldName.replace(new RegExp(/\[[0-9]+\]/), "[" + officeRowCount + "]");
+    $(this).attr('name', newName);
+  });
+
+  $newOfficeRow.insertAfter($lastOfficeRow);
+  bindRemovalButtons();
 }
 
-$(document).ready(function() {
+var bindRemovalButtons = function() {
+  $('.remove-office-row').unbind('click').on('click', function() {
+    $(this).closest('div.office-row-fieldset').remove();
+  });
+}
+
+var ready = function() {
   $('#add-office-row').on('click', function() {
     addOfficeRow();
   });
-});
+
+  $('.office-row-fieldset').first().find('.remove-office-row').hide();
+
+  bindRemovalButtons();
+}
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
